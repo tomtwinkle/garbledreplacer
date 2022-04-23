a9743a9cf2017569730d4694c04d466c6c50f7e7
tom twinkle <tomtinkle29@gmail.com>
2022-04-23 23:41:41 +0900

    add valid utf-8 check & runeError only string pass

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package garbledreplacer
 
 import (
-	"bytes"
+	"errors"
 	"unicode/utf8"
 
 	"golang.org/x/text/encoding"
 	"golang.org/x/text/transform"
 )
+
+var ErrInvalidUTF8 = errors.New("invalid UTF-8 character")
 
 func NewTransformer(enc encoding.Encoding, replaceRune rune) transform.Transformer {
 	e := enc.NewEncoder()
@@ -27,38 +29,38 @@ var _ transform.Transformer = (*replacer)(nil)
 
 func (t *replacer) Transform(dst, src []byte, atEOF bool) (nDst, nSrc int, err error) {
 	_src := src
-
 	if len(_src) == 0 && atEOF {
 		return
 	}
-	if !utf8.Valid(src) {
+	if !utf8.Valid(_src) {
 		// If not a string, do not process
-		n := copy(dst, src)
-		nDst = n
-		nSrc = n
+		err = ErrInvalidUTF8
 		return
 	}
 
-	var idx int
-	if len(_src) < len(dst) {
-		idx = len(_src)
-	} else {
-		idx = len(dst)
-	}
-	for _, r := range bytes.Runes(_src[:idx]) {
-		if r == utf8.RuneError {
-			continue
-		}
-		buf := []byte(string(r))
-		nSrc += len(buf)
+	for len(_src) > 0 {
+		_, n := utf8.DecodeRune(_src)
+		buf := _src[:n]
 		if _, err := t.enc.Bytes(buf); err != nil {
 			buf = []byte(string(t.replaceRune))
 		}
-		nDst += copy(dst[nDst:], buf)
+		if nDst+len(buf) > len(dst) {
+			// over destination buffer
+			err = transform.ErrShortDst
+			break
+		}
+		dstN := copy(dst[nDst:], buf)
+		if dstN <= 0 {
+			break
+		}
+		nSrc += n
+		nDst += dstN
+		_src = _src[n:]
 	}
 
-	if nDst < idx {
+	if len(_src) > len(dst) && nDst < len(dst) {
 		err = transform.ErrShortDst
 	}
+
 	return
 }
--- main_test.go
+++ main_test.go
@@ -9,6 +9,7 @@ import (
 	"golang.org/x/text/transform"
 	"strings"
 	"testing"
+	"unicode/utf8"
 )
 
 func TestNewTransformer(t *testing.T) {
@@ -36,6 +37,12 @@ func TestNewTransformer(t *testing.T) {
 			replace:  '@',
 			want:     strings.Repeat("一二三四@五六七八九@十拾壱", 3000),
 		},
+		"UTF-8->ShiftJIS:RuneError only": {
+			encoding: japanese.ShiftJIS,
+			in:       string(utf8.RuneError),
+			replace:  '?',
+			want:     "?",
+		},
 		"UTF-8->EUCJP:with garbled text": {
 			encoding: japanese.EUCJP,
 			in:       strings.Repeat("一二三四🍣五六七八九🍺十拾壱", 3000),
@@ -58,19 +65,19 @@ func TestNewTransformer(t *testing.T) {
 			var buf bytes.Buffer
 			w := transform.NewWriter(&buf, garbledreplacer.NewTransformer(tt.encoding, tt.replace))
 			if _, err := w.Write([]byte(tt.in)); err != nil {
-				panic(err)
+				t.Error(err)
 			}
 			if err := w.Close(); err != nil {
-				panic(err)
+				t.Error(err)
 			}
 
 			var actual bytes.Buffer
 			aw := transform.NewWriter(&actual, tt.encoding.NewDecoder())
 			if _, err := aw.Write(buf.Bytes()); err != nil {
-				panic(err)
+				t.Error(err)
 			}
 			if err := aw.Close(); err != nil {
-				panic(err)
+				t.Error(err)
 			}
 
 			if len([]rune(tt.want)) != len([]rune(actual.String())) {
